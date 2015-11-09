c7e8d4c6e5241acc9edcf4711ec1f42e70553b14
Michael Harris <mike.harris@cerner.com>
2015-11-09 17:40:03 -0600

    Added a check win function

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,5 +3,12 @@ module Lib
     ) where
 import Types
 import Game
+import Data.Matrix
+import qualified Data.Vector as V
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+--Use with matrix rows, columns, and diagonal as a list to determine if a player has won
+checkWin :: Eq a => [V.Vector a] -> Bool
+checkWin xs = any (== True) $ map (\v -> V.all (== V.head v) (V.tail v)) xs
