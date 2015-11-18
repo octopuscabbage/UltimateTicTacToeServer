0127102ddce086f61776928f0434f5f25ce34968
Michael <osiris2918@gmail.com>
2015-11-17 21:15:52 -0600

    Expanded win checking
    checkWin now matches against a square for determining a win.
    
    giveWinner determines who has won a board (if anyone)

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,5 +10,19 @@ someFunc = putStrLn "someFunc"
 
 
 --Use with matrix rows, columns, and diagonal as a list to determine if a player has won
-checkWin :: Eq a => [V.Vector a] -> Bool
-checkWin xs = any (== True) $ map (\v -> V.all (== V.head v) (V.tail v)) xs
+checkWin :: Square -> [V.Vector Square] -> Bool
+checkWin s xs = any (== True) $ map (\v -> V.all (\x -> x == s || x == Both) v) xs
+
+--Given a matrix of squares this will return which square (X, O, Neither, Both) has won
+giveWinner :: Matrix Square -> Square
+giveWinner m
+	| ((checkWin X items) && (checkWin O items)) = Both
+	| checkWin X items                           = X
+	| checkWin O items                           = O
+	| otherwise                                  = Empty
+	where 
+		rows = map (\x -> Data.Matrix.getRow x m) [1,2,3]
+		columns = map (\x -> Data.Matrix.getCol x m) [1,2,3]
+		leftDiag = [(Data.Matrix.getDiag m)]
+		rightDiag = [V.fromList [(Data.Matrix.getElem 3 1 m), (Data.Matrix.getElem 2 2 m), (Data.Matrix.getElem 1 3 m)]]
+		items = rows ++ columns ++ leftDiag ++ rightDiag
