b171ae8f205bd6edd55ababcf0ecadc336da2d9f
Michael <osiris2918@gmail.com>
2015-12-02 20:45:50 -0600

    Saving some changes
    Untested, stack died. Need to refresh.

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,7 +18,7 @@ checkWin s xs = any (== True) $ map (\v -> V.all (\x -> x == s || x == Both) v) 
 --Given a matrix of squares this will return which square (X, O, Neither, Both) has won
 giveWinner :: Matrix Square -> Square
 giveWinner m
-    | ((checkWin X items) && (checkWin O items)) = Both
+    | both                                       = Both
     | checkWin X items                           = X
     | checkWin O items                           = O
     | otherwise                                  = Empty
@@ -28,6 +28,7 @@ giveWinner m
         leftDiag = [(Data.Matrix.getDiag m)]
         rightDiag = [V.fromList [(Data.Matrix.getElem 3 1 m), (Data.Matrix.getElem 2 2 m), (Data.Matrix.getElem 1 3 m)]]
         items = rows ++ columns ++ leftDiag ++ rightDiag
+		both = ((checkWin X items) && (checkWin O items))
 
 --Given a game board, return a meta board showing state of all sub-boards
 checkSubBoards :: Matrix (Matrix Square) -> Matrix Square
@@ -39,11 +40,13 @@ updateBoard m (Move _ outer inner) s = setElem newInner ((div outer 3), (mod out
         innerBoard = getElem (div outer 3) (mod outer 3) m
         newInner = setElem s ((div inner 3), (mod inner 3)) innerBoard      
 
---updateGame :: Game -> Move -> Game
---updateGame gameBoard@(Game x o l b mB moveCount) move = Game {playerX = x, playerO = o, lastMove = move, board = newBoard, metaBoard = (checkSubBoards newBoard), moves = (moveCount + 1)}
---    where
---        newBoard = updateBoard b move player
---        player = getPlayer gameBoard move
+updateGame :: Game -> Move -> Game
+updateGame gameBoard@(Game x o l b mB moveCount winner) move = Game {playerX = x, playerO = o, lastMove = move, board = newBoard
+	, metaBoard = newMeta, moves = (moveCount + 1), gameWon = (giveWinner newMeta)}
+    where
+        newBoard = updateBoard b move player
+        player = getPlayer gameBoard move
+		newMeta = checkSubBoards newBoard
 
 getPlayer :: Game -> Move -> Square
 getPlayer (Game x o _ _ _ _ _) (Move p _ _)
